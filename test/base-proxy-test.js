28a2863f3236c621d1ad79ad6a027ad50e53e49d
Jason Gowan <jgowan@ziprecruiter.com>
2018-03-08 15:23:36 -0800

    pass chrome binary path to chromedriver

--- test/base-proxy-test.js
+++ test/base-proxy-test.js
@@ -71,6 +71,7 @@ describe('Proxy with chrome pool disabled', () => {
       'no-first-run',
       'no-sandbox',
     );
+    chromeOptions.setChromeBinaryPath('/usr/bin/google-chrome-beta');
     const options = chromeOptions.toCapabilities();
 
     driver = Driver.createSession('http://127.0.0.1:4444/wd/hub', options);
