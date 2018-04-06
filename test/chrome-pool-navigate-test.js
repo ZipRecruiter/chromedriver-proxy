53e14d3e6a4d9155a594d5381046c03dab721ad0
Jason Gowan <jgowan@ziprecruiter.com>
2018-04-06 14:56:21 -0700

    add page navigation

---
+++ test/chrome-pool-navigate-test.js
@@ -0,0 +1,100 @@
+/**
+* Copyright (c) 2017 ZipRecruiter
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* */
+
+
+const chai = require('chai');
+
+const expect = chai.expect;
+const HttpServer = require('..').HttpServer;
+
+const Driver = require('../clients/js/chrome_driver_proxy');
+const Chrome = require('selenium-webdriver/chrome').Driver;
+const ChromeOptions = require('selenium-webdriver/chrome').Options;
+
+const chromedriverBin = process.env.CHROMEDRIVER_BIN || '/usr/bin/chromedriver';
+
+// need chrome 64+ see https://bugs.chromium.org/p/chromium/issues/detail?id=767683
+describe.skip('Proxy with navigate', () => {
+  let server;
+  let driver;
+  const mockServerUrl = 'http://127.0.0.1:8080';
+
+  before(function (done) {
+    const config = {
+      proxy: {
+        port: 4444,
+        baseUrl: '/wd/hub',
+      },
+      chromedriver: {
+        chromedriverPath: chromedriverBin,
+        port: 4445,
+        autoRestart: false,
+      },
+      chromePool: {
+        enable: true,
+        reuse: true,
+        chromePath: '/usr/bin/google-chrome-beta',
+      },
+    };
+    server = new HttpServer(config.proxy);
+    this.timeout(5000);
+    server.start(config, done);
+  });
+
+  after((done) => {
+    server.stop(done);
+  });
+
+  beforeEach(() => {
+    const chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments(
+      'headless',
+      'disable-gpu',
+      'no-first-run',
+      'no-sandbox',
+    );
+    const options = chromeOptions.toCapabilities();
+
+    driver = Driver.createSession('http://127.0.0.1:4444/wd/hub', options);
+  });
+
+  afterEach((done) => {
+    driver.quit().then(() => { done(); });
+  });
+
+  it('can navigate with custom referrer', (done) => {
+    const referrer = 'https://google.com';
+    driver.pageNavigate({url: `${mockServerUrl}/headers`, referrer: referrer}).then(() => {
+        return driver.findElement({ css: '#referrer span.value' });
+      })
+      .then(elem => elem.getText())
+      .then((value) => {
+        expect(value).to.equal(referrer);
+      })
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
